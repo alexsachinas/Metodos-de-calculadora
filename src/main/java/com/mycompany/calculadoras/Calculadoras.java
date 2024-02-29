8a21c70b88f1446aebf363104ad8961165093be7
alexs <alexs@alexsachias09>
2024-02-28 19:37:11 -0600

    METODOS CALCULADORA para profesor alfonso

---
+++ src/main/java/com/mycompany/calculadoras/Calculadoras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoras;
+
+public class Calculadoras {
+
+    public static void main(String[] args) {
+        Operaciones op= new Operaciones();
+        op.leerNumeros();
+        op.sumar();
+        op.restar();
+        op.multiplicar();
+        op.division();
+        op.porcentaje();
+        op.mostarResultados();
+    }
+        
+    }
